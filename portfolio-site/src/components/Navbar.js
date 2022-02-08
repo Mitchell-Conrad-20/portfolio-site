9636cdd296ae301920668ad14282c3272b962029
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-02-08 15:42:07 -0500

    Started Navbar component
    -removed margin from body
    -created a Navbar component with a background color and an inline list
    
    -0 functionality, just trying to understand component behavior

---
+++ portfolio-site/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+const Navbar = () =>{
+    return(
+        <>
+            <ul>
+                <li>Test1</li>
+                <li>Test2</li>
+                <li>Test3</li>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar
