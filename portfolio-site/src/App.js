526645a3b0dd12dcc809bea910dd1bcc8a90ce9d
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-02-12 02:11:10 -0500

    Modal popup on initial load

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
 import Button from './components/Button.js';
+import Modal from './components/Modal.js';
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
-
+    
     <div className='content'>
       <h1>testing</h1>
       <Button linkTo='/'>testing</Button>
+      <Modal><h1 id='modalText'>Contact</h1></Modal>
+
     </div>
     </>
   );
--- portfolio-site/src/components/Button.js
+++ portfolio-site/src/components/Button.js
@@ -10,4 +10,4 @@ const Button = ( {children, linkTo} ) =>{
     )
 }
 
-export default Button
+export default Button;
